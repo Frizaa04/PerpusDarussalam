dde38368e6c6967bface7033edc34e32b335eec8
Machinaruu <muhammadadityan5@gmail.com>
2026-07-15 10:56:48 +0800

    feat: Searching

---
+++ PerpusDarussalam/app/Http/Controllers/SearchController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\category;
+use App\Models\Book;
+
+
+class SearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        $query = $request->input('q');
+
+        if (empty($query)) {
+            return view('layouts.search-result', [
+                'books' => collect([]),
+                'categories' => category::all()
+            ]);
+        }
+
+        $books = Book::query()->where(function($keywordQuery) use ($query) {
+            $keywordQuery->where('judul', "%{$query}%")
+                         ->orWhere('penerbit', "%{$query}%");
+        })->get();
+
+        return view('layouts.search-result', [
+            'books' => $books,
+            'categories' => category::all()
+        ]);
+    }
+}
